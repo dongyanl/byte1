87a1d0d56f57528d7f9fcd0b2bca3cbc94ac9a79
Dongyang Lu <dongyanl@andrew.cmu.edu>
2018-01-25 19:26:07 -0500

    added libraries

---
+++ main.py
@@ -0,0 +1,14 @@
+# Imports
+import os
+import jinja2
+import webapp2
+import logging
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+    
+@app.route('/')
+def hello():
+    template = JINJA_ENVIRONMENT.get_template('templates/index.html')
+    return template.render()
